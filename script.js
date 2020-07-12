4f83784e70909b189a896dcc9d7eac7a9fc2b2b2
alexandredev3 <costasalexandre511@gmail.com>
2020-07-12 17:55:58 -0300

    message form send finished

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ function log(message) {
 // App
 const fields = document.querySelectorAll('[required]');
 // Estou pegando todas as tags que tem o atributo required.
+const boxMessage = document.querySelector('.box-message');
+
+console.log(fields);
+
+// Para cada field de dentro de fields vou fazer...
+for (let field of fields) {
+  field.addEventListener('invalid', event => {
+    // O "invalid" fica disponivel quando temos o required como atributo.
+    log("campo invalido")
+  });
+};
 
 
 
@@ -22,11 +33,18 @@ const fields = document.querySelectorAll('[required]');
 
 
 
+function sendForm() {
+  log('Formulario enviado com sucesso!');
+  boxMessage.classList.add('active');
+  boxMessage.classList.remove('desability');
 
+  setTimeout(() => {
+    boxMessage.classList.remove('active');
+    boxMessage.classList.add('desability');
+  }, 3000);
 
+  event.preventDefault();
+}
 
 document.querySelector('form')
-.addEventListener('submit', event => {
-  log('Formulario enviado com sucesso!');
-  event.preventDefault();
-});
+.addEventListener('submit', event => sendForm(event));

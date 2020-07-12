bba19ad1ebac7b2a197ff65efa20da99f06d432b
alexandredev3 <costasalexandre511@gmail.com>
2020-07-12 18:33:15 -0300

    required name successfully changed

--- script.js
+++ script.js
@@ -10,13 +10,56 @@ const boxMessage = document.querySelector('.box-message');
 
 console.log(fields);
 
+// Estou pegando esse event da estrutura de repetição for.
+function customValidation(event) {
+  const field = event.target;
+
+  // console.log(field.validity);
+  // retorna um objeto com varios valores.
+  // quando eu coloco o setCustomValidity, ele coloca o field.validity -> customError como true.
+  // se eu não tiver uma logica de limpar o setCustomValidity ele sempre vai estar com erro de validação.
+
+  // --> logica para verificar se existem error no field.validity. (limpar o setCustomValidity) <--
+  function verifyErrors() {
+    let foundError = false;
+    
+    for(let error in field.validity) {
+      // console.log(field.validity[error]);
+      // assim eu vou saber o valor da propriedade.
+
+      // se não for customError.(o custom error não intereça porque ele sempre vai ser true)
+      // então verifica se tem erro
+      if (error != 'customError' && field.validity[error]) {
+        foundError = error
+        // aqui eu estou pegando o erro que deu
+      }
+      /*Estou verificando se o error NÃO e o customError, e se o field.validity[error] 
+        e igual a true, se for aprovado essa condição e porque tem erro!!
+      */
+    }
+
+    return foundError;
+  };
+
+  const error = verifyErrors();
+  log(`Error exists: ${error}`)
+
+  // se tiver error pode rodar o field.setCustomValidity
+  if (error) {
+    // --> Mudar message do required <--
+    field.setCustomValidity('Esse campo é obrigatório!');
+  } else {
+    field.setCustomValidity('');
+  };
+
+};
+// O "target" e o field do for
+
 // Para cada field de dentro de fields vou fazer...
 for (let field of fields) {
-  field.addEventListener('invalid', event => {
-    // O "invalid" fica disponivel quando temos o required como atributo.
-    log("campo invalido")
-  });
+  field.addEventListener('invalid', customValidation);
 };
+// O "invalid" fica disponivel quando temos o required como atributo.
 
 
 
@@ -33,7 +76,7 @@ for (let field of fields) {
 
 
 
-function sendForm() {
+function sendForm(event) {
   log('Formulario enviado com sucesso!');
   boxMessage.classList.add('active');
   boxMessage.classList.remove('desability');
@@ -47,4 +90,4 @@ function sendForm() {
 }
 
 document.querySelector('form')
-.addEventListener('submit', event => sendForm(event));
+.addEventListener('submit', sendForm);
--- text-node.md
+++ text-node.md
@@ -6,3 +6,31 @@
   - Quando for colocar o script no head, podemos colocar o *defer*, ele vai esperar
   todo o conteudo ser carregado pra depois carregar o script, evitando dá algum 
   problema.
+
+```js
+  field.setCustomValidity('Esse campo é obrigatório!');
+```
+  - Se nos apenas colocar essa função para mudar a message de required do input,
+  ele vai retorna a messange de required com o campo preenchido ou não, então
+  precisamos criar uma logica.
+
+## Diferença do for in e do for of
+
+```js
+  let lists = [
+    { id: 1, name: 'fazer cafe', status: 'feito' },
+    { id: 2, name: 'estudar programção', status: 'não feito'},
+    { id: 3, name: 'fazer um lanche', status: 'não feito' },
+    { id: 4, name: 'dormir', status: 'não feito' }
+  ];
+
+  --> Ele vai operar em cada uma das listas separadamente...
+  for (list of lists) {
+    ...
+  }
+
+  --> Ele vai operar em cada propriedade da minha lista...
+  for (name of list) {
+    ...
+  }
+```

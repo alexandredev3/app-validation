f6e08325c7908b19403623955428a04d5d58c664
alexandredev3 <costasalexandre511@gmail.com>
2020-07-12 19:34:40 -0300

    project finished

--- script.js
+++ script.js
@@ -10,16 +10,8 @@ const boxMessage = document.querySelector('.box-message');
 
 console.log(fields);
 
-// Estou pegando esse event da estrutura de repetição for.
-function customValidation(event) {
-  const field = event.target;
-
-  // console.log(field.validity);
-  // retorna um objeto com varios valores.
-  // quando eu coloco o setCustomValidity, ele coloca o field.validity -> customError como true.
-  // se eu não tiver uma logica de limpar o setCustomValidity ele sempre vai estar com erro de validação.
-
-  // --> logica para verificar se existem error no field.validity. (limpar o setCustomValidity) <--
+function ValidateField(field) {
+  // Logica para verificar se existem erros
   function verifyErrors() {
     let foundError = false;
     
@@ -29,64 +21,127 @@ function customValidation(event) {
 
       // se não for customError.(o custom error não intereça porque ele sempre vai ser true)
       // então verifica se tem erro
-      if (error != 'customError' && field.validity[error]) {
+      if (field.validity[error] && !field.validity.valid) {
         foundError = error
         // aqui eu estou pegando o erro que deu
       }
-      /*Estou verificando se o error NÃO e o customError, e se o field.validity[error] 
-        e igual a true, se for aprovado essa condição e porque tem erro!!
+      /*Estou verificando se o error NÃO e o customError, e se o !field.validity.valid
+      for false
       */
+
+      // verificando qual error ele encontrou.
+      // console.log(foundError);
     }
 
     return foundError;
   };
 
-  const error = verifyErrors();
-  log(`Error exists: ${error}`)
+  // console.log(field.validity);
 
-  // se tiver error pode rodar o field.setCustomValidity
-  if (error) {
-    // --> Mudar message do required <--
-    field.setCustomValidity('Esse campo é obrigatório!');
-  } else {
-    field.setCustomValidity('');
+  // aqui eu estou passando como parametro qual e o tipo do error.
+  function customMessage(typeError) {
+    const messages = {
+      text: {
+        // quando o error for valueMissing eu vou retornar essa mensagem
+        valueMissing: 'Por favor, preencha este campo'
+      },
+      email: {
+        valueMissing: 'Email é obrigatório',
+        typeMismatch: 'Por favor, preencha um email válido'
+      }
+    }
+
+    return messages[field.type][typeError];
+    // ou seja se o field type for text ele vai retorn se o field type for email ele vai pegar.
+    // typeError ele vai retorna o tipo do error.
   };
+
+  // console.log(field.validity);
+  // retorna um objeto com varios valores.
+  // quando eu coloco o setCustomValidity, ele coloca o field.validity -> customError como true.
+  // se eu não tiver uma logica de limpar o setCustomValidity ele sempre vai estar com erro de validação.
+
+  function setCustomMessage(message) {
+    const spanError = field.parentNode.querySelector('span.error');
+    // estou subindo pro pai(.input) e fazendo um querySelector no span.error.
+    
+    // se tiver message vai entrar nesse if.
+    if (message) {
+      spanError.classList.add('active');
+      spanError.innerHTML = message; 
+    } else {
+      spanError.classList.remove('active');
+      spanError.innerHTML = '';
+    };
+  };
+
+  // aqui eu estou registrando essa função.
+  return function() {
+
+    const error = verifyErrors();
+
+    // se tiver error ele vai entrar aqui.
+    if(error) {
+      const message = customMessage(error);
+
+      field.style.borderColor = '#ff5858';
+      setCustomMessage(message);
+    } else {
+      field.style.borderColor = '#08ca79';
+      setCustomMessage();
+    };
+  };
+};
+
+// Estou pegando esse event da estrutura de repetição for.
+function customValidation(event) {
+
+
+
+  const field = event.target;
+
+  const validation =  ValidateField(field);
+
+  // esse validation e aquela função que nos registramos no ValidateField
+  validation();
+
+  // console.log(validation);
+
+  // console.log(ValidateField(field));
+
+
+  // log(`Error exists: ${error}`);
+
 
 };
 // O "target" e o field do for
 
 // Para cada field de dentro de fields vou fazer...
 for (let field of fields) {
-  field.addEventListener('invalid', customValidation);
+  field.addEventListener('invalid', event => {
+    // como so vai ter o bubble no invalid, faz sentido colocar ele aqui.
+
+    // exterminar o bubble do html padrão.
+    event.preventDefault();
+    // com esse codigo, nossa missão de exterminar o bubble ja esta concluido. 
+
+    customValidation(event)
+  });
+  field.addEventListener('blur', customValidation)
+  // O event blur e quando ele perder o focodo, quando ele não esta mais selecionado.
 };
 // O "invalid" fica disponivel quando temos o required como atributo.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function sendForm(event) {
-  log('Formulario enviado com sucesso!');
-  boxMessage.classList.add('active');
+  event.preventDefault();
+
+  boxMessage.classList.add('actived');
   boxMessage.classList.remove('desability');
 
   setTimeout(() => {
-    boxMessage.classList.remove('active');
+    boxMessage.classList.remove('actived');
     boxMessage.classList.add('desability');
   }, 3000);
-
-  event.preventDefault();
 }
 
 document.querySelector('form')
